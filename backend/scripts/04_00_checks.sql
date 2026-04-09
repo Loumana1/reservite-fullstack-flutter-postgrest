071c86ebfc68b43f97bc4cf2d6cf730e745ed605
sam_prophete <0609sansengimana@student.epfc.eu>
2026-04-09 17:20:45 +0200

    (users_tables) contarint et trigger

--- backend/scripts/04_00_checks.sql
+++ backend/scripts/04_00_checks.sql
@@ -1,6 +1,8 @@
-set search_path to public ;
-
+--*************************************************************************************************
+                                 --USERS--
+--*************************************************************************************************
 alter table users
+
     add constraint users_full_name_min_length
     check ( length(trim(full_name)) >= 3) ,
 
@@ -9,4 +11,17 @@ alter table users
                 and password ~ '[0-9]'
                 and password ~ '[A-z]'
                 and password ~ '[a-z]'
-                and password ~ '[]')
+                and password ~ '[,;.:!?/$%&@#]');
+
+
+--**************************************************************************************************
+                                 --TABLES--
+--***************************************************************************************************
+
+alter table tables
+    add constraint table_number_positive
+    check ( table_number > 0) ,
+
+    add constraint table_capacity_positive
+    check ( capacity > 0 );
+
--- backend/scripts/04_40_trigger_tables.sql
+++ backend/scripts/04_40_trigger_tables.sql
@@ -1,9 +1,19 @@
-create or replace function table_unique()
+--************************************************************************
+                        --TABLES--
+--************************************************************************
+
+create or replace function table_unique_restaurant()
 
 returns trigger as $$
     begin
-        if 
+        if old.restaurant != new.restaurant then
+            raise exception 'The restaurant of the table cannot be changed';
+        end if;
+        return new;
     end;
+    $$ language plpgsql;
 
-
-    $$
+create trigger table_unique_restaurant
+    before update on tables
+    for each row
+execute function table_unique_restaurant();
