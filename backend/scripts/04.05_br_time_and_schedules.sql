fb9694a7cb9fe373210bd818ffc2360fd542dd8b
zietraore <2610zitraore@student.epfc.eu>
2026-04-16 15:51:31 +0200

    feat(triggers): ajout trigger differe our br4 et br5

--- backend/scripts/04.05_br_time_and_schedules.sql
+++ backend/scripts/04.05_br_time_and_schedules.sql
@@ -19,7 +19,7 @@ CREATE OR REPLACE FUNCTION is_time_aligned_on_slot(time_val TIME, slot_mins INT)
        --Br 5 here de res inclus dans servie du jour
 
 
-CREATE OR REPLACE FUNCTION trg_check_br04_br05_reservation_time() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION trg_check_reservation_time() RETURNS TRIGGER AS $$
 DECLARE
 res_slot_duration INT;
     res_day_of_week INT;
@@ -35,11 +35,11 @@ IF NOT is_aligned_on_slot(NEW.datetime, res_slot_duration) THEN
         RAISE EXCEPTION 'L''heure de la réservation n''est pas alignée sur la durée des créneaux du restaurant .', res_slot_duration;
 END IF;
 
-    -- -br4
+    --br4
     day_name := TO_CHAR(NEW.datetime, 'FMDay');
 
     IF day_name = 'Monday'      THEN res_day_of_week := 1; END IF;
-    IF day_name = 'Tuesday'   THEN res_day_of_week := 2; END IF;
+    IF day_name = 'Tuesday'   THEN res_day_of_week := 2z; END IF;
     IF day_name = 'Wednesday' THEN res_day_of_week := 3; END IF;
     IF day_name = 'Thursday'  THEN res_day_of_week := 4; END IF;
     IF day_name = 'Friday'   THEN res_day_of_week := 5; END IF;
@@ -65,6 +65,7 @@ RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER br_04_05_res_time
-    BEFORE INSERT OR UPDATE ON reservations
-FOR EACH ROW EXECUTE FUNCTION trg_check_br04_br05_reservation_time();
+create constraint trigger br_04_05_res_time
+    before insert or  update
+    on reservations
+    for each row execute function trg_check_reservation_time();
