af872cd5a88d40720578223633c3f92fdd30ab98
sam_prophete <0609sansengimana@student.epfc.eu>
2026-05-14 13:49:17 +0200

    (build) model table

---
+++ frontend/lib/model/table.dart
@@ -0,0 +1,13 @@
+class table {
+
+  final int id;
+  final int resaurantId;
+  final int tableNumble;
+  final int capacity;
+
+  table({required this.id, required this.resaurantId, required this.tableNumble, required this.capacity});
+
+  factory table.json(){
+
+  }
+}
