e4f9c01d4cd2c03d5e0898e1c17db9ed07577bc4
zietraore <2610zitraore@student.epfc.eu>
2026-05-17 18:59:07 +0200

    refactor(triggers):BR-11; renomage: fichier, fonctions, message d'erreur

--- backend/scripts/04.11_br_reservation_termine_ou_annulee.sql
+++ backend/scripts/04.11_br_reservation_termine_ou_annulee.sql
@@ -1,8 +1,8 @@
-create or replace function tgr_check_modification_reservation()
+create or replace function tgr_check_reservation_terminee_ou_annulee()
 returns trigger as $$
     begin
         if old.status in ('completed', 'cancelled')
-            then raise exception 'Interdit ! Cette reservation est deja % et ne peut plus etre touche. ', old.status;
+            then raise exception 'Interdit ! Cette reservation est deja % et ne peut plus etre modifiée.', old.status;
         end if;
 
         return new;
@@ -12,4 +12,4 @@ returns trigger as $$
 create or replace trigger trigger_modification_reservation
     before update on reservations
     for each row
-    execute  function tgr_check_modification_reservation();
+    execute  function tgr_check_reservation_terminee_ou_annulee();
