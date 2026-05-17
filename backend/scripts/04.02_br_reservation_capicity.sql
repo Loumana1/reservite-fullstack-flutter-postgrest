30bc4a15458f5ddcb826644574feaa69395aad11
zietraore <2610zitraore@student.epfc.eu>
2026-05-17 16:06:30 +0200

    fix(triggers):BR-02; ajout de logique afin que le triggers accepte des entrées a partir de plusieurs tables en utilisaant tg_table_name et tg_op

--- backend/scripts/04.02_br_reservation_capicity.sql
+++ backend/scripts/04.02_br_reservation_capicity.sql
@@ -3,42 +3,56 @@ returns trigger as $$
     declare
         total_capacity int ;
         res_guests int ;
-        res_status text;
-        res_restaurant_id int;
-        inv_table_count int;
+        res_status varchar;
+        res_id int;
+
     begin
+        if tg_table_name = 'reservations' then
+            res_id:= new.id ;
+            res_guests := new.number_of_guests;
+            res_status := new.status;
+        elsif tg_table_name = 'reservation_tables' then
 
-        select number_of_guests, status , restaurant
-        into res_guests , res_status , res_restaurant_id
-        from reservations
-        where id = new.reservation;
-
-        if res_status in ('confirmed' , 'completed') then
-            select count(*) into inv_table_count
-            from reservation_tables rt
-            join tables t on rt.table = t.id
-            where rt.reservation = new.reservation
-            and t.restaurant != res_restaurant_id;
-
-            if inv_table_count > 0 then
-                raise exception 'plusiers table n''appartiennent pas au restaurant de la reservation !';
-
+            if tg_op = 'delete' then
+                res_id:=old.reservation;
+                else
+               res_id:= new.reservation;
             end if;
 
-            select sum(capacity) into total_capacity
-            from tables
-            where id in (select  "tables" from reservation_tables where reservation = new.reservation)
-            or id = new.table;
 
-            if total_capacity < res_guests then
-                raise exception 'Capacite insuffusante : % places pour % convives.', total_capacity , res_guests;
-            end if;
+            select number_of_guests, status into res_guests , res_status
+            from reservations
+            where id = res_id;
+        elsif tg_table_name = 'tables' then
+            return null;
         end if;
-        return  new ;
+
+
+             if res_status in ('confirmed' , 'completed') then
+                select coalesce(sum(capacity),0) into total_capacity
+                from tables t join reservation_tables rt on t.id = rt."table"
+                where rt.reservation =res_id;
+
+                if total_capacity < res_guests then
+                    raise exception 'Capacite insuffusante : % places pour % convives.', total_capacity , res_guests;
+                end if;
+            end if;
+        return  null ;
     end;
     $$language  plpgsql;
 
-create trigger trigger_check_capacity
-    before insert or update  on reservation_tables
-    for each row
-    execute function tgr_check_reservation_capacity();
+
+drop trigger if exists trigger_check_capacity on reservation_tables;
+drop trigger if exists trigger_check_capacity on reservations;
+
+create constraint trigger  trigger_check_capacity
+   after insert or update  on reservations
+    deferrable  initially deferred
+    for each row execute function tgr_check_reservation_capacity();
+
+
+
+create constraint trigger trigger_check_capacity
+    after insert or update or delete on reservation_tables
+    deferrable initially deferred
+    for each row execute function tgr_check_reservation_capacity();
