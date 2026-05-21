a80235b39d7a4d84df7f95e6c3a6868763f84376
sam_prophete <0609sansengimana@student.epfc.eu>
2026-05-21 16:05:24 +0200

    (build) Apiclient et model securite

--- frontend/lib/core/services/api_client.dart
+++ frontend/lib/core/services/api_client.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
+import 'dart:js_interop';
 import 'package:http/http.dart' as http;
+import '../tools/params.dart';
 
 String get baseUrl => kisWeb || !Platform.isAndroid
     ? 'http://localhost:3000/rpc'
@@ -8,9 +10,30 @@ String get baseUrl => kisWeb || !Platform.isAndroid
 class ApiClient {
   static final http.Client _client = http.Client();
 
-  static Future<http.Response> get() async {}
+  static Future<http.Response> get(String endpoint, {Map<String, String>?
+  headers, bool anonymous = false}) async {
+    final Uri url = Uri.parse('$baseUrl/$endpoint');
+    return await _client.get(
+      url,
+      headers: {
+        ...?headers,
+        'Content-type' : 'application/json; charset=UTF-8',
+        if (!anonymous) 'Authorization': 'Bearer ${Params.getValue('token')}'
+      },
+    );
+  }
 
-  static Future<http.Response> post() async {}
-
-
+  static Future<http.Response> post(String endpoint, {Map<String, String>?
+  headers, dynamic body, bool anonymous = false}) async {
+    final Uri url = Uri.parse('$baseUrl/$endpoint');
+    return await _client.post(
+      url,
+      headers: {
+        ...?headers,
+        'Content-Type': 'application/json; charset=UTF-8',
+        if (!anonymous) 'Authorization': 'Bearer ${Params.getValue('token')}',
+      },
+      body : body,
+    );
+  }
 }
---
+++ frontend/lib/model/security.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:prbd_2526_c06/core/services/api_client.dart';
+
+class Security {
+
+  static Future<String?> login(String email, String password) async {
+    final response = await ApiClient.post(
+      'login' ,
+      body: json.encode({'email': email, 'password': password}),
+      anonymous: true,
+    );
+    if (response.statusCode != 200) throw Exception('Echec de connexion');
+    return json.decode(response.body)['token'];
+  }
+
+  static Future<void> signup(String fullName, String email , String password) async {
+    final response = await ApiClient.post(
+      'signup' ,
+      body: json.encode({'full_name': fullName, 'email' : email, 'password' : password}),
+      anonymous: true
+    );
+    if (response.statusCode != 204) throw Exception('Echec de l\'inscription');
+  }
+}
