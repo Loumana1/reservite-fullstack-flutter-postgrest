ec6fcb07ceb6dcb7c0b3ab96ce7ef425456af275
zietraore <2610zitraore@student.epfc.eu>
2026-05-21 16:33:14 +0200

    fix(triggers): reparation des erreur de connection

--- backend/scripts/04.12_br_reservation_table_for_pending_or_cancelled.sql
+++ backend/scripts/04.12_br_reservation_table_for_pending_or_cancelled.sql
@@ -13,7 +13,10 @@ returns trigger as $$
     end;
     $$ language plpgsql;
 
-create or replace trigger trigger_table_assigment
-    before insert or update on reservation_tables
+drop trigger if exists trigger_table_assigment on reservation_tables;
+
+create constraint trigger trigger_table_assigment
+    after insert or update on reservation_tables
+    deferrable  initially deferred
     for each row
-    execute function tgr_check_reservation_table_for_pending_or_cancelled()
+    execute function tgr_check_reservation_table_for_pending_or_cancelled();
