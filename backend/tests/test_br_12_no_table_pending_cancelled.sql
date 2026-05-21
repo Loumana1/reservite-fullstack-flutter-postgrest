24ebebffad667b15521a6b0b48b7dca5bc6b98f0
zietraore <2610zitraore@student.epfc.eu>
2026-05-21 16:00:14 +0200

    feat(triggers): ajout test positif BR-12

--- backend/tests/test_br_12_no_table_pending_cancelled.sql
+++ backend/tests/test_br_12_no_table_pending_cancelled.sql
@@ -5,11 +5,24 @@ do
 $test$
 begin
 
-        raise notice 'TEST: BR-12, Une réservation en attente ou annulée ne peut être associée à aucune une table.';
+        raise notice 'TEST: BR-12 should fail, Une réservation en attente ou annulée ne peut être associée à aucune une table.';
         perform should_fail($$
             insert into reservation_tables (reservation, "table")
             values (2, 4);
         $$);
 end
+$test$;
+rollback;
+
+------- essai test positif
+begin;
+do
+$test$
+    begin
+
+            raise notice 'TEST: BR-12 positif,  Une réservation en attente ou annulée ne peut être associée à aucune une table.';
+                insert into reservation_tables (reservation, "table")
+                values (13, 3);
+    end
 $test$;
 rollback;
